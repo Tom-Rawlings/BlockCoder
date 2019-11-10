244023b5a3699114fab453e6858f5d7ee925f424
Tom Rawlings <tom-115@hotmail.co.uk>
2019-11-10 22:50:42 +0000

    Cleanup files

--- js/lightModeToggle.js
+++
@@ -1,7 +0,0 @@
-function toggleLightMode(){
-	if(document.getElementById("lightModeToggle").checked == true){
-		document.body.classList.add("darkMode");
-	}else{
-		document.body.classList.remove("darkMode");
-	}
-}
